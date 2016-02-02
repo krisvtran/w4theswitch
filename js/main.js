8b0dbfe701091ec0e0cc9779786b5c7bfabd02af
Graphic <graphic@SOD-SJH-605-20M.local>
2016-02-02 15:20:24 -0500

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+$('button').on('click', function(){
+	$('.on').toggleClass('off');
+
+	if ($('button').hasClass('off')){
+		$('body').addClass('dark');
+		$('.status').text("Hey, who turn off the lights")
+	}
+
+	else {
+		$('body').removeClass('dark');
+		$('.status').text("It's so bright in here!")
+	}
+
+});
 
 
